539e8076645f1aa0005ed608d717817f88532636
na7esan <na7esan@gmail.com>
2020-12-08 11:44:22 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ Route::get('/', 'TasksController@index');
 
 Route::resource('tasks', 'TasksController');
 
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 // // CRUD
 // // メッセージの個別詳細ページ表示
 // Route::get('tasks/{id}', 'TasksController@show');
